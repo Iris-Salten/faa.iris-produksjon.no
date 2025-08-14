e20d8380c8da22bf894d57da6c29e3cc5a9d6e7d
voie420 <mathiasvj@outlook.com>
2025-08-14 12:52:54 +0200

    Fixing the api endpoint to handle search parameters better.

--- app/api/deklarasjoner/route.ts
+++ app/api/deklarasjoner/route.ts
@@ -1,9 +1,20 @@
 import axios from 'axios';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const page = searchParams.get('page') || '1';
+  const size = searchParams.get('size') || '50';
+  const search = searchParams.get('search');
+
+  const url = new URL('api/Deklarasjoner', process.env.API_BASE_URL);
+
+  if (page) url.searchParams.set('PageNumber', page);
+  if (size) url.searchParams.set('PageSize', size);
+  if (search && search.length > 0) url.searchParams.set('FreeText', search);
+
   const response = await axios
-    .get(process.env.API_BASE_URL + `/Deklarasjoner`, {
+    .get(url.href, {
       headers: {
         Cookie: `Avdekl.Cookie=${process.env.API_AUTH_KEY}`,
       },
