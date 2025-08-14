55eaf62d5f6c9b0d2b382a95006855aa07c4e107
voie420 <mathiasvj@outlook.com>
2025-08-14 12:52:22 +0200

    Moving topContent to the header parallell route.

--- app/(pages)/deklarasjoner/[guid]/[version]/visning/page.tsx
+++ app/(pages)/deklarasjoner/[guid]/[version]/visning/page.tsx
@@ -3,87 +3,44 @@
 import moment from 'moment';
 import { Context } from './layout';
 import { useContext } from 'react';
-import {
-  Button,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownTrigger,
-} from '@heroui/react';
-import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function Page() {
   const data = useContext(Context);
 
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
   return (
-    <>
-      <>
-        <span className="flex w-full items-center justify-between">
-          <h1 className="font-semibold text-2xl">Se deklarasjon</h1>
-          <Dropdown>
-            <DropdownTrigger>
-              <Button isIconOnly variant="light" size="sm">
-                <svg width={25} height={25} viewBox="0 0 640 640">
-                  <path d="M544 320C544 346.5 522.5 368 496 368C469.5 368 448 346.5 448 320C448 293.5 469.5 272 496 272C522.5 272 544 293.5 544 320zM368 320C368 346.5 346.5 368 320 368C293.5 368 272 346.5 272 320C272 293.5 293.5 272 320 272C346.5 272 368 293.5 368 320zM144 368C117.5 368 96 346.5 96 320C96 293.5 117.5 272 144 272C170.5 272 192 293.5 192 320C192 346.5 170.5 368 144 368z" />
-                </svg>
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu>
-              <DropdownItem
-                key="1"
-                onPress={() => {
-                  const params = new URLSearchParams(searchParams);
-                  params.set('action', 'view');
-                  params.set('type', 'history');
-
-                  router.push('?' + params);
-                }}
-              >
-                Historikk
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </span>
-      </>
-      <>
-        <header className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full rounded-xl bg-green-500/15 p-5 gap-5 md:gap-1">
-          <div className="flex flex-col">
-            <p className="font-medium">Dato opprettet</p>
-            <p>
-              {moment(data?.eidAv?.overfoeringTidspunkt)
-                .add(2, 'hours')
-                .format('DD.MM.YY, HH:mm')}
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <p className="font-medium">Deklarasjonsnummer</p>
-            <p>
-              {data?.deklarasjonsnummer
-                .toString()
-                .split(/(?<=^(?:.{3})+)(?!$)/)
-                .join('.')}
-            </p>
-          </div>
-          {data?.eidAv?.anlegg?.forvaltetAv ? (
-            <div className="flex flex-col">
-              <p className="font-medium">Mottak</p>
-              <p>{data?.eidAv?.anlegg?.forvaltetAv?.navn}</p>
-            </div>
-          ) : (
-            <div className="flex flex-col">
-              <p className="font-medium">Produsent</p>
-              <p>{data?.eidAv?.organisasjon?.navn}</p>
-            </div>
-          )}
-          <div className="flex flex-col">
-            <p className="font-medium">Avfallsstoffnummer</p>
-            <p>{data?.avfall?.farligAvfallsstoffnummer?.kode || '-'}</p>
-          </div>
-        </header>
-      </>
-    </>
+    <header className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full rounded-xl bg-green-500/15 p-5 gap-5 md:gap-1">
+      <div className="flex flex-col">
+        <p className="font-medium">Dato opprettet</p>
+        <p>
+          {moment(data?.eidAv?.overfoeringTidspunkt)
+            .add(2, 'hours')
+            .format('DD.MM.YY, HH:mm')}
+        </p>
+      </div>
+      <div className="flex flex-col">
+        <p className="font-medium">Deklarasjonsnummer</p>
+        <p>
+          {data?.deklarasjonsnummer
+            .toString()
+            .split(/(?<=^(?:.{3})+)(?!$)/)
+            .join('.')}
+        </p>
+      </div>
+      {data?.eidAv?.anlegg?.forvaltetAv ? (
+        <div className="flex flex-col">
+          <p className="font-medium">Mottak</p>
+          <p>{data?.eidAv?.anlegg?.forvaltetAv?.navn}</p>
+        </div>
+      ) : (
+        <div className="flex flex-col">
+          <p className="font-medium">Produsent</p>
+          <p>{data?.eidAv?.organisasjon?.navn}</p>
+        </div>
+      )}
+      <div className="flex flex-col">
+        <p className="font-medium">Avfallsstoffnummer</p>
+        <p>{data?.avfall?.farligAvfallsstoffnummer?.kode || '-'}</p>
+      </div>
+    </header>
   );
 }
--- app/(pages)/deklarasjoner/page.tsx
+++ app/(pages)/deklarasjoner/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useQuery } from '@tanstack/react-query';
-import { useParams, useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+import moment from 'moment';
 
 import {
   Chip,
@@ -15,25 +16,37 @@ import {
   TableRow,
 } from '@heroui/react';
 
-import moment from 'moment';
-
 export default function Deklarasjoner() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const { page, size } = useParams<{ page: string; size: string }>();
+  const page = searchParams.get('page');
+  const size = searchParams.get('size');
+  const search = searchParams.get('search');
 
   const { data, isPending, isRefetching } = useQuery<DeklarasjonerResultDto>({
-    queryKey: ['deklarasjoner'],
+    queryKey: ['deklarasjoner', page, size, search],
     staleTime: 1000 * 60 * 5,
     queryFn: async () => {
-      return await fetch(
-        `/api/deklarasjoner?page=${page || 1}&size=${size || 50}`,
-      ).then((res) => res.json());
+      const url = new URL('/api/deklarasjoner', window.location.origin);
+
+      if (search && search.length > 0) {
+        url.searchParams.set('search', search);
+      }
+
+      if (page) {
+        url.searchParams.set('page', page);
+      }
+
+      if (size) {
+        url.searchParams.set('size', size);
+      }
+
+      return await fetch(url).then((res) => res.json());
     },
   });
 
-  if (isPending || isRefetching) {
+  if (isPending || isRefetching || !data) {
     return (
       <div className="flex w-full items-center justify-center h-[500px]">
         <Spinner />
@@ -42,16 +55,12 @@ export default function Deklarasjoner() {
   }
 
   return (
-    <span className="flex flex-col w-full gap-3">
-      <h1 className="font-semibold text-2xl">Alle deklarasjoner</h1>
-
-      {data && (
-        <Table
-          removeWrapper
-          classNames={{
-            tr: 'hover:bg-gray-100 transition-all cursor-pointer',
-          }}
-          bottomContent={
+    <Table
+      aria-label="Table list of all declarations"
+      removeWrapper
+      bottomContent={
+        data?.totalPages > 0 && (
+          <div className="flex items-center justify-center">
             <Pagination
               onChange={(newPage) => {
                 const params = new URLSearchParams(searchParams);
@@ -59,55 +68,66 @@ export default function Deklarasjoner() {
                 router.push('?' + params);
               }}
               showControls
-              page={1}
-              total={10}
+              page={data.currentPage}
+              total={data.totalPages}
             />
-          }
-        >
-          <TableHeader>
-            <TableColumn>Dek. nr.</TableColumn>
-            <TableColumn>Status endret</TableColumn>
-            <TableColumn>Avfallsnr. og beskrivelse</TableColumn>
-            <TableColumn>Mengde</TableColumn>
-            <TableColumn>Mottak</TableColumn>
-            <TableColumn>Status</TableColumn>
-          </TableHeader>
-          <TableBody>
-            {data.deklarasjoner.map((deklarasjon, index) => (
-              <TableRow
-                href={`/deklarasjoner/${deklarasjon.deklarasjonsnummer}/${deklarasjon.versjon}/visning`}
-                key={index}
-              >
-                <TableCell>{deklarasjon.deklarasjonsnummer}</TableCell>
-                <TableCell className="whitespace-nowrap">
-                  {moment(deklarasjon?.eidAv?.overfoeringTidspunkt).format(
-                    'DD.MM.YY HH:mm',
-                  )}
-                </TableCell>
-                <TableCell>
-                  <p className="font-semibold text-sm">
-                    {deklarasjon?.avfall?.farligAvfallsstoffnummer?.kode}
-                  </p>
-                  <p className="text-sm">
-                    {deklarasjon?.avfall?.farligAvfallsstoffnummer?.navn}
-                  </p>
-                </TableCell>
-                <TableCell>
-                  {deklarasjon.mengdeBrutto} {deklarasjon.maaleenhet}
-                </TableCell>
-                <TableCell className="text-blue-500">
-                  {deklarasjon?.eidAv?.anlegg?.navn}
-                </TableCell>
-                <TableCell>
-                  <Chip size="sm">
-                    {deklarasjon?.currentStatus?.statusDefinition}
-                  </Chip>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      )}
-    </span>
+          </div>
+        )
+      }
+      classNames={{
+        tr: 'hover:bg-gray-100 transition-all cursor-pointer',
+      }}
+    >
+      <TableHeader>
+        <TableColumn>Dek. nr.</TableColumn>
+        <TableColumn>Status endret</TableColumn>
+        <TableColumn>Avfallsnr. og beskrivelse</TableColumn>
+        <TableColumn>Mengde</TableColumn>
+        <TableColumn>Mottak</TableColumn>
+        <TableColumn>Status</TableColumn>
+      </TableHeader>
+      <TableBody
+        emptyContent={
+          <p className="text-sm text-gray-500">
+            {search
+              ? 'Ingen deklarasjoner ble funnet for det valgte søket eller filteret.'
+              : 'Ingen deklarasjoner ble funnet.'}
+          </p>
+        }
+      >
+        {data?.deklarasjoner.map((deklarasjon, index) => (
+          <TableRow
+            href={`/deklarasjoner/${deklarasjon.deklarasjonsnummer}/${deklarasjon.versjon}/visning`}
+            key={index}
+          >
+            <TableCell>{deklarasjon.deklarasjonsnummer}</TableCell>
+            <TableCell className="whitespace-nowrap">
+              {moment(deklarasjon?.eidAv?.overfoeringTidspunkt).format(
+                'DD.MM.YY HH:mm',
+              )}
+            </TableCell>
+            <TableCell>
+              <p className="font-semibold text-sm">
+                {deklarasjon?.avfall?.farligAvfallsstoffnummer?.kode}
+              </p>
+              <p className="text-sm">
+                {deklarasjon?.avfall?.farligAvfallsstoffnummer?.navn}
+              </p>
+            </TableCell>
+            <TableCell>
+              {deklarasjon.mengdeBrutto} {deklarasjon.maaleenhet}
+            </TableCell>
+            <TableCell className="text-blue-500">
+              {deklarasjon?.eidAv?.anlegg?.navn}
+            </TableCell>
+            <TableCell>
+              <Chip size="sm">
+                {deklarasjon?.currentStatus?.statusDefinition}
+              </Chip>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
